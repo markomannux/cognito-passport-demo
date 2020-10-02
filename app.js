c351ecc5007883d0cdc35eb234ec37628ee8b96e
Marco <marco.mannucci@hey.com>
2020-10-02 22:50:41 +0200

    Demo project completed

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 require('dotenv').config();
+const fetch = require('node-fetch');
+const Headers = require('node-fetch').Headers;
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var jwt = require('jsonwebtoken');
 
 var passport = require('passport');
 var OAuth2Strategy = require('passport-oauth2');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { post } = require('./routes/index');
 
 var app = express();
 
+const POOL_BASE_URL = process.env.POOL_BASE_URL;
 const AUTH_URL = process.env.AUTH_URL; 
 const TOKEN_URL = process.env.TOKEN_URL;
 const EXAMPLE_CLIENT_ID = process.env.EXAMPLE_CLIENT_ID;
@@ -24,16 +29,16 @@ const CALLBACK_URL = process.env.CALLBACK_URL;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
 }))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -47,6 +52,23 @@ passport.deserializeUser(function(user, done) {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+OAuth2Strategy.prototype.userProfile = async function(accessToken, done) {
+var myHeaders = new Headers();
+myHeaders.append("Authorization", `Bearer ${accessToken}`);
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+const profile = await fetch(`${POOL_BASE_URL}/oauth2/userInfo`, requestOptions)
+  .then(response => response.text())
+  .then(result => done(null, result))
+  .catch(error => console.log('error', error));
+  return profile;
+}
+
 passport.use(new OAuth2Strategy({
     authorizationURL: AUTH_URL,
     tokenURL: TOKEN_URL,
@@ -55,15 +77,28 @@ passport.use(new OAuth2Strategy({
     callbackURL: CALLBACK_URL
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log('Access token', accessToken)
-    console.log('Refresh token', refreshToken)
-    console.log('Profile', profile);
-    return cb(null, {dummyUser: 'pippo'});
+    let decoded = jwt.decode(accessToken);
+    console.log(profile);
+    return cb(null, profile);
   }
 ));
 
+function isLoggedIn(request, response, next) {
+    // passport adds this to the request object
+    if (request.isAuthenticated()) {
+        return next();
+    }
+    response.redirect('/login');
+}
+
 app.use('/', indexRouter);
-app.use('/users', passport.authenticate('oauth2', { scope: 'email' }), usersRouter);
+app.get('/login', passport.authenticate('oauth2',
+    {
+      scope: ['openid'],
+      successRedirect: '/users',
+      failureRedirect: '/'
+    }))
+app.use('/users', isLoggedIn, usersRouter);
 
 app.get('/auth/example/callback', passport.authenticate('oauth2', { failureRedirect: '/' }), (req, res) => {
   res.send('Auth completed')
